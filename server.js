184a8b953caf4ec2a897cbefe95dff67fe048ba3
samarthweb <113467681+samarthweb@users.noreply.github.com>
2022-10-06 15:01:34 -0400

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get("/", function(req, res){
     let resText = "<h2>Declaration (Instruction: text size in heading 2):</h2> <br>The rest text is displayed in paragraph as shown in screenshot.<br>";
     resText += "<br>I acknowledge the College's academic integrity policy and my own integrity remain in effect whether my work is<br>done remotely or onsite. Any test or assignment is an act of trust between me and my instructor, and especially with<br>my classmates... even when no one is watching. I declare I will not break that trust.<br>"; 
     resText += "<br>Name: Samarth Sunilkumar Patel<br><br>Student Number:150061208<br>"; 
-    resText += "<br>click<br>"
+    resText += "<br>click to visit CPA students<br>"
     resText += "<br>click to see who has the highest GPA<br>"
     res.send(resText);
 });
