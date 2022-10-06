7e73a40300e1adc54794a69bd917812de4fb9cff
samarth2199 <74289161+samarth2199@users.noreply.github.com>
2022-10-06 14:45:23 -0400

    y

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.get("/", function(req, res){
     resText += "<br>I acknowledge the College's academic integrity policy and my own integrity remain in effect whether my work is<br>done remotely or onsite. Any test or assignment is an act of trust between me and my instructor, and especially with<br>my classmates... even when no one is watching. I declare I will not break that trust.<br>"; 
     resText += "<br>Name: Samarth Sunilkumar Patel<br><br>Student Number:150061208<br>"; 
     resText += "<br>click<br>"
+    resText += "<br>click to see who has the highest GPA<br>"
     res.send(resText);
 });
 
